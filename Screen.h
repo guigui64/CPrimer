30131a1d1d759964eda86863f27b37e05de1f9ef
guigui64 <guillaume.comte10@gmail.com>
2016-05-18 17:52:13 +0200

    Beautifier

--- Screen.h
+++ Screen.h
@@ -11,10 +11,13 @@ class Screen {
 public:
     typedef std::string::size_type pos;
     Screen() = default; // needed because Screen has another constructor
-    Screen(pos ht, pos wd, char c): height(ht), width(wd), contents(ht * wd, c) {}
+    Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht * wd, c) {
+    }
     char get() const // here, 'const' means that the content of Screen cannot be changed
                      // (except for mutable members)
-    { return contents[cursor]; }           // implicitly inline
+    {
+        return contents[cursor];
+    }                                      // implicitly inline
     inline char get(pos ht, pos wd) const; // explicitly inline
     Screen &move(pos r, pos c);            // can be made inline later
     Screen &set(char);
